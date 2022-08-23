07180d37512a829534f3867171ca5988ccdf052c
Alok-712001-Baluni <alokbaluni712001@gmail.com>
2022-08-23 14:29:57 +0530

    __proto__, getPrototypeOF and setPortotypeOf

--- index.js
+++ index.js
@@ -1,87 +1,28 @@
-// & lexical scope defines how variable names are resolved in nested functions. 
+// & Prototypal Inheritance and prototypal Chain
 
-// & Nested(child) functions have access to the scope of their parent functions.
- 
-// &  This is often confused with closures but lexical scope is only as important 
-// & part of closure.
-
-// ! global scope
-
-// & A closure is a function having access to the parent scope,
-// ^ "even after the parent function has closed" 
-
-// ? A closure is created when we define a function, not when a function is executed. 
-
-/*
-let x = 10;
-
-const parentFunction = ()=>{
-    //! local scope
-    let myValue = 20;
-    console.log(x)
-    console.log(myValue);
-
-        const childFunction = ()=>{
-            console.log(x+=1);
-            console.log(myValue+=2);
-        }
-        return childFunction;
+//? Object literals
+const person = {
+    alive: true
 }
 
-const result = parentFunction();
-console.log(result);
+const musician = {
+    plays: true
+}
 
-result(); // ? even though the function is closed the child function still has 
-//? access to the variable of the parent function.
-result(); // ? myValue variable is private in the global scope(so can't log it in he console here).
+/*
+musician.__proto__ = person; // ? old way of telling that person is the parent of the musician
+
+console.log(musician);
+console.log(musician.plays);
+console.log(musician.alive);  
+
 
 */
 
+//? new way :-
 
-
-
-/*
-
-
-
-// & IIFE (Immmediately Invoked function expression)
-
-// & privateCounter returns only one time.
-const privateCounter = (() =>{
-    let count = 0;
-    console.log(` initial value ${count}`);
-
-    return () => { 
-    count += 1; 
-    console.log(count);
-}
-})();
-
-privateCounter();
-privateCounter();
-privateCounter();
-privateCounter();
-privateCounter();
-privateCounter();
-
-*/
-
-
-
-const credits = ((num) => {
-    let credits = num;
-    console.log(`initial credits value: ${credits}`);
-    return () => {
-        credits -= 1;
-        if(credits > 0){
-            console.log(`playing game, ${credits} remaining`);
-        } else{
-            console.log('not enough credits left');
-        }
-    }
-})(3);
-
-// ? anonymous function has the closure over the IIFE.
-credits();
-credits();
-credits();
+Object.setPrototypeOf(musician, person); // ? musician is the child of person
+console.log(Object.getPrototypeOf(musician)); //! {alive: true}
+console.log(musician);
+console.log(Object.getPrototypeOf(musician) === musician.__proto__); //! true
+console.log(musician.alive) // ! true, javascript has to walk up the proto chain.
